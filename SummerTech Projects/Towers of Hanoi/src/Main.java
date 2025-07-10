c2a3d7608c608efa9735970b8678a41fbce1d905
rbabjb2 <sblbrbabjb2@gmail.com>
2025-07-10 17:31:07 +0000

    Coded up to dark blue, TODO later.

--- SummerTech Projects/Towers of Hanoi/src/Main.java
+++ SummerTech Projects/Towers of Hanoi/src/Main.java
@@ -10,12 +10,45 @@ public class Main {
         for (int i = 0; i < towerHeight; i++) {
             left.add(i);
         }
-        
+
+
+        moveTop2(center, left, right);
+
+        move(right, left);
+        //Green 
+        moveTop2(right, center, left);
+
+        move(center, left);
+        //Light Blue
+
+        moveTop2(left, right, center);
+
+        move(center, right);
+
+        moveTop2(center, left, right);
+
+        move();
+        // Dark Blue
+        mo
+
         System.out.println(left);
+        System.out.println(center);
+        System.out.println(right);
     }
 
     public static void main(String[] args) {
         new Main();
+    }
+
+    public void move(ArrayList<Integer> end, ArrayList<Integer> start) {
+        end.add(0, start.get(0));
+        start.remove(0);
+    }
+
+    public void moveTop2(ArrayList<Integer> to, ArrayList<Integer> from, ArrayList<Integer> other) {
+        move(other, from);
+        move(to, from);
+        move(to, other);
     }
 
 }
